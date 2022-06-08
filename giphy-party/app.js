1226b435456c9fa9757a20cdcde46afd16f547e3
hayley <simmons.hayley15@icloud.com>
2022-06-08 16:18:27 -0500

    Steps 1-2 Finished
    Built website layout & search form; edited page in CSS

---
+++ giphy-party/app.js
@@ -0,0 +1,20 @@
+// item references
+
+// header div
+const headerDivElement = document.getElementById("header-Div")
+const headerH1Element = document.getElementById("H1-header")
+
+// form
+const formTagElement = document.getElementById("Form-Tag")
+const searchLabelElement = document.getElementById("Search-Label")
+const searchBarElement = document.getElementById("Search-Bar")
+const searchButtonElement = document.getElementById("Search-Button")
+
+// results div
+const resultsDivElement = document.getElementById("Results-Div")
+// -----------------------------------------
+
+// 
+
+// -----------------------------------------
+
