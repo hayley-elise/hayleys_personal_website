985e94b3051b536fa5c25a8a668dd4ce7087bc0c
hayley <simmons.hayley15@icloud.com>
2022-06-08 21:04:44 -0500

    Steps 3-5 Finished
    Step 6 Incomplete

--- giphy-party/app.js
+++ giphy-party/app.js
@@ -1,20 +1,52 @@
-// item references
+// -----------------------------------------
+// global constants
 
-// header div
-const headerDivElement = document.getElementById("header-Div")
-const headerH1Element = document.getElementById("H1-header")
+// API endpoints
+const q = ""                                                // search phrase/word
+const limit = 8                                             // number of search result objects to return
+const offset = 0                                            // starting position of results
+const rating = "g"                                          // content rating
+const lang = "en"                                           // default language
+const api_key = "z9qEJKCAcrrMCA7q77qHJXLDkv111Kra"          // API key
+// search
+const apiUrl = `http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${q}`.toLowerCase()
 
 // form
-const formTagElement = document.getElementById("Form-Tag")
-const searchLabelElement = document.getElementById("Search-Label")
-const searchBarElement = document.getElementById("Search-Bar")
-const searchButtonElement = document.getElementById("Search-Button")
+const form_Element = document.getElementById("Form-Tag")
+const searchLabel_Element = document.getElementById("Search-Label")
+const searchBar_Element = document.getElementById("Search-Bar")
+const searchButton_Element = document.getElementById("Search-Button")
 
 // results div
-const resultsDivElement = document.getElementById("Results-Div")
-// -----------------------------------------
-
-// 
+const resultsDiv_Element = document.getElementById("results-Div")
 
 // -----------------------------------------
+// results logic
 
+const getResults = form_Element.addEventListener("submit", async (API_results) => {
+    API_results.preventDefault()
+
+    let response = await fetch(apiUrl)
+    let responseData = await response.json()
+
+    displayResults(responseData)
+})
+
+// -----------------------------------------
+// display logic
+
+function displayResults(responseData) {
+    responseData.forEach(resultsDiv_Element => {
+        imgURL = responseData.images.fixed_height.url
+        resultsDiv_Element.innerHTML +=  `
+        <img src="${imgURL}" alt="Result GIF" /> 
+    `
+    }) 
+}
+
+// -----------------------------------------
+// search form submit logic
+
+form_Element.addEventListener("submit", (submission) => {
+    
+})
